6b9a96d45080580c934e0ca88262f37059151d12
Jakub Janczak <jakub.janczak@schibsted.pl>
2016-01-04 23:22:41 +0100

    Reading property from file

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+
+const PropertiesReader = require('properties-reader');
+
 module.exports = {
     main: function(event, context) {
-              console.log("Hello world");
+              const properties = PropertiesReader('env.properties');
+              console.log("Hello world. Property A from properties == " + properties.get('A'));
               context.succeed("done");
           }
 }
